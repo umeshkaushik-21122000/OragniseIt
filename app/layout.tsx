5f6245989c792c37212c5766899c73427130360d
umeshkaushik21 <kaushikumesh21@gmail.com>
2024-02-20 18:00:00 +0530

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,15 @@ import { Toaster } from "sonner";
 import { Inter } from 'next/font/google'
 import type { Metadata } from 'next'
 
-
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { EdgeStoreProvider } from "@/lib/edgestore";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'organiseit',
+  title: 'Jotion',
   description: 'The connected workspace where better, faster work happens.',
   icons: {
     icon: [
